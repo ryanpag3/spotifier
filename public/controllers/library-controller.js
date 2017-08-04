2531f6fc56d78fb1a02717c37e85cc302a507103
Ryan Page <ryanapage@gmail.com>
2017-08-04 16:02:37 -0700

    added helper function for sorting user libraries by artist name, release title, and release date and applied it to create $scope variables for each sorted library, need to add ui elements to trigger displaying different libraries

--- public/controllers/library-controller.js
+++ public/controllers/library-controller.js
@@ -1,21 +1,22 @@
 var app = angular.module('spotifier');
 app.controller('library-controller', ['$scope', '$rootScope', '$timeout', 'libraryService',
     function($scope, $rootScope, $timeout, libraryService) {
-        init();
+
 
         // sort by date added
         $scope.library = [];
         $scope.libraryReversed = {};
         // sort by artist
-        $scope.libraryArtistAscending = {};
-        $scope.libraryArtistDescending = {};
+        $scope.libraryArtistAscending = [];
+        $scope.libraryArtistDescending = [];
         // sort by release title
-        $scope.libraryReleaseTitleAscending = {};
-        $scope.libraryReleaseTitleDescending = {};
+        $scope.libraryTitleAscending = [];
+        $scope.libraryTitleDescending = [];
         // sort by release date
-        $scope.libraryReleaseDateAscending = {};
-        $scope.libraryReleaseDateDescending = {};
+        $scope.libraryDateAscending = [];
+        $scope.libraryDateDescending = [];
 
+        init();
         $scope.syncLibrary = function() {
             libraryService.sync();
         };
@@ -23,7 +24,57 @@ app.controller('library-controller', ['$scope', '$rootScope', '$timeout', 'libra
             libraryService.get()
                 .then(function(library) {
                     $scope.library = library;
+                    $scope.libraryArtistAscending = sortBy(library, 'name');
+                    $scope.libraryArtistDescending = (sortBy(library, 'name')).reverse();
+                    $scope.libraryTitleAscending = sortBy(library, 'title');
+                    $scope.libraryTitleDescending = (sortBy(library, 'title')).reverse();
+                    $scope.libraryDateAscending = sortBy(library, 'date');
+                    $scope.libraryDateDescending = (sortBy(library, 'date')).reverse();
                 });
         }
 
-}]);
+        // sorting utility for user libraries
+        function sortBy(array, key) {
+            switch (key) {
+                case 'name':
+                    return array.sort(function(a,b) {
+                        var aName = a.name.toLowerCase();
+                        var bName = b.name.toLowerCase();
+
+                        if (aName < bName){
+                            return -1;
+                        }
+                        if (aName > bName){
+                            return 1;
+                        }
+                        return 0;
+                    }).slice();
+                case 'title':
+                    return array.sort(function(a,b) {
+                        var aTitle = a.recent_release.title.toLowerCase();
+                        var bTitle = b.recent_release.title.toLowerCase();
+
+                        if (aTitle < bTitle){
+                            return -1;
+                        }
+                        if (aTitle > bTitle){
+                            return 1;
+                        }
+                        return 0;
+                    }).slice();
+                case 'date':
+                    return array.sort(function(a,b) {
+                        var aDate = new Date(a.recent_release.release_date);
+                        var bDate = new Date(b.recent_release.release_date);
+                        if (aDate < bDate) {
+                            return -1;
+                        }
+                        if (aDate > bDate) {
+                            return 1;
+                        }
+                        return 0;
+                    }).slice();
+            }
+        }
+    }
+]);
