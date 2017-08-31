a1ba195e8d4825a5382d4050bd185c6777e4fe9b
Ryan Page <ryanapage@gmail.com>
2017-08-31 16:07:43 -0700

    setting up node_env check for switching callback urls

--- server/server.js
+++ server/server.js
@@ -17,6 +17,6 @@ require('./app.js')(app, express, socket); // expose middleware/setup applicatio
 
 httpServer.listen(3000, function () {
     console.log('Server successfully launched on port 3000');
-    console.log(process.env.NODE_ENV);
+    console.log('node_env=' + process.env.NODE_ENV);
 });
 
