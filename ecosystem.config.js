443d07be5032515c75c0331c5754e72a35bbfa2c
root <root@spotifier.io>
2017-08-31 19:17:36 -0400

    ecosystem change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/ryanpage42/spotifier.git',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
 };
