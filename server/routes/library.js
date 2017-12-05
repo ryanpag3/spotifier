62f11907355606f795a3c843bf4c9c83ea4e4aae
Ryan Page <ryanapage@gmail.com>
2017-12-05 07:33:04 -0800

    small change

--- server/routes/library.js
+++ server/routes/library.js
@@ -16,10 +16,10 @@ var express = require('express'),
 /**
  * returns the authenticated user's tracked artist library
  */
-router.get('/update', function(req, res) {
+router.get('/update', function (req, res) {
     var db = new Db();
     db.getLibrary(req.user)
-        .then(function(library) {
+        .then(function (library) {
             return res.status(200).json({
                 library: library
             })
@@ -30,18 +30,18 @@ router.get('/update', function(req, res) {
  * This will sync the users library artists with the database, and return an updated list
  * for the client.
  */
-router.get('/sync', function(req, res) {
+router.get('/sync', function (req, res) {
     refreshAccessToken(req)
-        .then(function() {
+        .then(function () {
             syncLibraryJobQueue.createJob(req.user)
-                .catch(function(err) {
+                .catch(function (err) {
                     return res.status(500).json({
                         err: err
-                        })
+                    })
                 });
             res.status(200).send();
         })
-        .catch(function(err) { // catch refresh access token error
+        .catch(function (err) { // catch refresh access token error
             console.log(err);
         });
 });
@@ -50,30 +50,30 @@ router.get('/sync', function(req, res) {
  * Calls the sync library queue utility to attempt to remove
  * the job for the specified user from the queue.
  */
-router.get('/cancel-sync', function(req, res) {
-   syncLibraryJobQueue.removeJob(req.user)
-       .then(function() {
-           res.status(200).send();
-       })
-       .catch(function(err) {
-           return res.status(500).json({
-               err: err
-           })
-       })
+router.get('/cancel-sync', function (req, res) {
+    syncLibraryJobQueue.removeJob(req.user)
+        .then(function () {
+            res.status(200).send();
+        })
+        .catch(function (err) {
+            return res.status(500).json({
+                err: err
+            })
+        })
 });
 
 /**
  * This endpoint returns the current sync queue status for the
  * authenticated user in a JSON object.
  */
-router.get('/sync-status', function(req, res) {
+router.get('/sync-status', function (req, res) {
     syncLibraryJobQueue.getJobStatus(req.user)
-        .then(function(status) {
+        .then(function (status) {
             return res.status(200).json({
                 status: status
             })
         })
-        .catch(function(err) {
+        .catch(function (err) {
             return res.status(500).json({
                 err: err
             })
@@ -88,29 +88,29 @@ router.get('/sync-status', function(req, res) {
  * we use the database wrapper to associate the artist with the
  * user.
  */
-router.post('/add', function(req, res) {
+router.post('/add', function (req, res) {
     var socketUtil = req.app.get('socketio');
     var db = new Db();
     // refresh a users access token if necessary
     refreshAccessToken(req)
-        .then(function() {
+        .then(function () {
             // query users collection for user information
             db.getUser(req.user)
-                .then(function(user) {
+                .then(function (user) {
                     // add artist
                     db.addArtist(user, req.body.artist, socketUtil)
-                        .then(function() {
+                        .then(function () {
                             // return success code
                             res.status(200).send();
                         })
-                        .catch(function(err) {
+                        .catch(function (err) {
                             // return fail code with error message
                             return res.status(500).json({
                                 err: err
                             })
                         });
                 })
-                .catch(function(err) {
+                .catch(function (err) {
                     // user does not exist in database, redirect to landing page
                     req.redirect('/');
                 })
@@ -122,14 +122,14 @@ router.post('/add', function(req, res) {
  * a specific user. Calls the database wrapper which returns
  * a promise that we use to return a status code.
  */
-router.post('/remove', function(req, res) {
+router.post('/remove', function (req, res) {
     var deferred = Q.defer(),
         db = new Db();
     db.removeArtist(req.user, req.body.artist)
-        .then(function() {
+        .then(function () {
             res.status(200).send();
         })
-        .catch(function(err) {
+        .catch(function (err) {
             return res.status(200).json({
                 err: err
             })
@@ -141,28 +141,30 @@ router.post('/remove', function(req, res) {
  * API endpoint for returning the currently authenticated
  * user information.
  */
-router.get('/me', function(req, res) {
+router.get('/me', function (req, res) {
     return res.status(200).json({
         user: req.user
     })
+});
+
+router.get('/scan', function (req, res) {
+    releaseScanner.startScan(true);
+    res.status(200).send();
 });
 
 /**
  * Expose these routes only if in development mode
  */
 if (!process.env.NODE_ENV) {
-    router.get('/scan', function(req, res) {
-        releaseScanner.startScan(true);
-        res.status(200).send();
-    });
 
-    router.get('/validate', function(req, res) {
+
+    router.get('/validate', function (req, res) {
         var db = new Db();
         db.validateArtistDetails();
         res.status(200).send();
     })
 
-    router.get('/playlist', function(req,res) {
+    router.get('/playlist', function (req, res) {
         var playlist = require('../utils/playlist-handler');
         playlist.updateNewReleasePlaylists();
         res.status(200).send();
@@ -179,12 +181,12 @@ function refreshAccessToken(req) {
     var api = new SpotifyApiUser(),
         deferred = Q.defer();
     api.getAccessToken(req.user)
-        .then(function(accessToken) {
-            if (accessToken){
+        .then(function (accessToken) {
+            if (accessToken) {
                 // access token was expired, need to refresh
                 req.session.passport.user.accessToken = accessToken;
-                req.session.save(function(err) {
-                    if (err){
+                req.session.save(function (err) {
+                    if (err) {
                         console.log(err);
                     }
                     deferred.resolve();
@@ -195,12 +197,10 @@ function refreshAccessToken(req) {
                 deferred.resolve();
             }
         })
-        .catch(function(err) {
+        .catch(function (err) {
             deferred.reject(err);
         });
     return deferred.promise;
 }
 
 module.exports = router;
-
-
