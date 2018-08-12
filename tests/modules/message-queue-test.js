11c9014e70ea1e17f5fe4776fd6e42a450484c38
Ryan Page <ryanapage@gmail.com>
2018-08-12 15:49:33 -0700

    message queue changes

--- tests/modules/message-queue-test.js
+++ tests/modules/message-queue-test.js
@@ -1,9 +1,18 @@
 const messageQueue = require('message-queue');
+const expect = require('chai').expect;
 const RSMQPromise = require('rsmq-promise');
 
 describe('message-queue.js', () => {
     it ('should create a artist details message', (done) => {
         messageQueue.createArtistDetailsJob('1234', 'token')
-            .then(() => done());
+            .then(() => messageQueue.consumeArtistDetailsJob())
+            .then((message) => {
+                expect(message).to.not.be.undefined;
+                done();
+            });
     });
+
+    it ('should create a sync-library message', (done) => {
+        
+    })
 });
