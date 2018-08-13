dc867cbc6de8080d540f2a582b521ca42d6c045b
Ryan Page <ryanapage@gmail.com>
2018-08-13 09:23:50 -0700

    reset access token logic and test

--- tests/modules/spotify-api-test.js
+++ tests/modules/spotify-api-test.js
@@ -4,9 +4,9 @@ const expect = require('chai').expect;
 let SpotifyApi = require('spotify-api');
 
 describe('spotify-api-test.js', () => {
+    let REFRESH_TOKEN = pConfig.spotify.testRefreshToken;
     it('should set a valid access token with a valid refresh token', (done) => {
-        let refreshToken = pConfig.spotify.testRefreshToken;
-        let api = new SpotifyApi(refreshToken);
+        let api = new SpotifyApi(REFRESH_TOKEN);
         api.initialize()
             .then(() => {
                 try {
@@ -28,6 +28,24 @@ describe('spotify-api-test.js', () => {
                     expect(api.accessToken).to.not.be.undefined;
                     expect(api.api).to.not.be.undefined;
                     expect(api.tokenExpiresAt).to.not.be.undefined;
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+    });
+
+    it ('should grant a new token when the current one is expired', (done) => {
+        let api = new SpotifyApi();
+        api.initialize()
+            .then(() => {
+                // now take a step back and slide real smooth
+                api.tokenExpiresAt = new Date(api.tokenExpiresAt.getTime() - 3601);
+                return api.getAccessToken();
+            })
+            .then(() => {
+                try {
+                    expect(api.tokenExpiresAt).to.be.greaterThan(new Date());
                     done();
                 } catch (e) {
                     done(e);
