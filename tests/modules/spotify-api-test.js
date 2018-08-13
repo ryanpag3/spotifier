8b898ef72cb34881cf3defb13872768de2241a1b
Ryan Page <ryanapage@gmail.com>
2018-08-13 08:47:20 -0700

    continue spotify api object definitions

--- tests/modules/spotify-api-test.js
+++ tests/modules/spotify-api-test.js
@@ -1,10 +1,37 @@
 const pConfig = require('../../private/config-private');
 const logger = require('logger');
+const expect = require('chai').expect;
 let SpotifyApi = require('spotify-api');
 
 describe('spotify-api-test.js', () => {
-    it('should set a valid access token with a valid refresh token', () => {
+    it('should set a valid access token with a valid refresh token', (done) => {
         let refreshToken = pConfig.spotify.testRefreshToken;
         let api = new SpotifyApi(refreshToken);
+        api.initialize()
+            .then(() => {
+                try {
+                    expect(api.accessToken).to.not.be.undefined;
+                    expect(api.api).to.not.be.undefined;
+                    expect(api.tokenExpiresAt).to.not.be.undefined;
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+    });
+
+    it('should set a valid access token without a refresh token', (done) => {
+        let api = new SpotifyApi();
+        api.initialize()
+            .then(() => {
+                try {
+                    expect(api.accessToken).to.not.be.undefined;
+                    expect(api.api).to.not.be.undefined;
+                    expect(api.tokenExpiresAt).to.not.be.undefined;
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
     });
 });
