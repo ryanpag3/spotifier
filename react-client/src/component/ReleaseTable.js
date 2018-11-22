f6e92673884d2a964f118cb09f9caeeac550b2d5
Ryan Page <ryanapage@gmail.com>
2018-11-22 11:15:35 -0800

    add library filtering, add date from hover text

--- react-client/src/component/ReleaseTable.js
+++ react-client/src/component/ReleaseTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { List, AutoSizer } from 'react-virtualized';
+import moment from 'moment';
 import ReactJson from 'react-json-view';
 import libraryAPI from '../api/libraryAPI';
 import './ReleaseTable.css';
@@ -22,34 +23,6 @@ export default class ReleaseTable extends Component {
         this.setState({ library: newProps.library });
     }
 
-    renderRow({ index, key, style }) {
-        return (
-            <div key={key} style={style} className="row">
-                <div className="row-container flex-row center-vert">
-                    <div className="album-img-background">
-                        <img className="album-img" src={this.getRecentReleaseImg(this.state.library[index])} alt=""/>
-                    </div>
-                    <div className="album-info flex-col center-vert">
-                        <div className="release">
-                            <a href={this.state.library[index].recent_release.url}>
-                                {this.state.library[index].recent_release.title}
-                            </a>
-                        </div>
-                        <div className="name">
-                            <a href={this.state.library[index].url}>
-                                {this.state.library[index].name}
-                            </a>
-                        </div>
-                    </div>
-                    <div className="release-date-container align-right">
-                        <div className="release-date">{new Date(this.state.library[index].recent_release.release_date).toLocaleDateString()}</div>
-                    </div>
-                </div>
-
-            </div>
-        );
-    }
-
     getRecentReleaseImg(release) {
         if (!release.recent_release){
             return DummyAlbumArt;
@@ -62,7 +35,42 @@ export default class ReleaseTable extends Component {
         const imgs = release.recent_release.images;
         return imgs[imgs.length-1].url;
         
-    };
+    }
+
+    calcHowLongAgo(releaseDate) {
+        const now = moment();
+        return new moment(releaseDate).from(now);
+    }
+
+    renderRow({ index, key, style }) {
+        return (
+            <div key={key} style={style} className="row">
+                <div className="row-container flex-row center-vert">
+                    <div className="album-img-background">
+                        <img className="album-img" src={this.getRecentReleaseImg(this.state.library[index])} alt=""/>
+                    </div>
+                    <div className="album-info flex-col center-vert">
+                        <div className="release">
+                            <a href={this.state.library[index].recent_release.url} target="_blank" rel="noopener noreferrer">
+                                {this.state.library[index].recent_release.title}
+                            </a>
+                        </div>
+                        <div className="name">
+                            <a href={this.state.library[index].url} target="_blank" rel="noopener noreferrer">
+                                {this.state.library[index].name}
+                            </a>
+                        </div>
+                    </div>
+                    <div className="release-date-container align-right">
+                        <div className="release-date" 
+                                title={this.calcHowLongAgo(this.state.library[index].recent_release.release_date)}>
+                                {new Date(this.state.library[index].recent_release.release_date).toLocaleDateString()}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     render() {
         return (
--- react-client/src/container/Library.js
+++ react-client/src/container/Library.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Input } from 'semantic-ui-react';
+import Fuse from 'fuse.js';
 import Button from '@material-ui/core/Button';
 import LibraryApi from '../api/libraryAPI';
 import ReleaseTable from '../component/ReleaseTable';
@@ -10,24 +12,61 @@ class Library extends Component {
         this.state = {
             library: []
         };
+
+        // this.searchArtists = this.searchArtists.bind(this);
     };
 
     async initialize() {
         console.log('initializing user library');
         const payload = await LibraryApi.get();
         this.setState({ library: payload ? payload.library : [] });
+        this.setState({ masterLibrary: payload ? payload.library : [] }); // backup for filtering
     }
 
     async componentDidMount() {
         this.initialize();
     }
 
+    filterLibrary(filterQuery) {
+        if (filterQuery === '') {
+            this.resetLibraryToMaster();
+            return;
+        }
+
+        const options = {
+            shouldSort: true,
+            threshold: 0.6,
+            location: 0,
+            distance: 100,
+            maxPatternLength: 32,
+            minMatchCharLength: 1,
+            keys: [
+                'name',
+                'recent_release.title'
+            ]
+        }
+
+        const fuse = new Fuse(this.state.masterLibrary, options);
+        const result = fuse.search(filterQuery);
+        this.setState({library: result});
+    }
+
+    searchArtists(e) {
+        if (e.keyCode !== 13) return;
+        this.filterLibrary(e.target.value);
+    }
+
+    resetLibraryToMaster() {
+        this.setState({library: this.state.masterLibrary})
+    }
+
     render() {
         return (
-            <div>
+            <div className="library-container">
                 <Button variant="contained" onClick={LibraryApi.sync}>
                     Sync Library
-                </Button><br/><br/>
+                </Button>
+                <Input icon='search' placeholder='Search...' onKeyDown={(e) => this.searchArtists(e)} onChange={(e) => this.filterLibrary(e.target.value)}/>
                 <ReleaseTable library={this.state.library}/>
             </div>
         );
